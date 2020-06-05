e0f3c9f369e04d71aaec71ee71c206e628cabd3f
rahulnarayanan033 <rahulnarayanan033@gmail.com>
2020-06-05 11:55:56 +0530

    Single Page Application Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}){
         return (
@@ -13,51 +14,46 @@ function RenderDish({dish}){
         )
     }
 
-function RenderComments({dish}){
-        var months = ["January", "February", "March", "April", "May", "June",
-                        "July", "August", "September", "October", "November", "December"];
-        var d0 = new Date(dish.comments[0].date)
-        var d1 = new Date(dish.comments[1].date)
-        var d2 = new Date(dish.comments[2].date)
-        var d3 = new Date(dish.comments[3].date)
-        var d4 = new Date(dish.comments[4].date)
-           return(
-               <div>
-                   <h4>Comments</h4>
-                   <ul className="list-unstyled">
-                       <li>{dish.comments[0].comment}</li><br></br>
-            	        <li>-- {dish.comments[0].author}  {months[d0.getMonth()]} {d0.getDate()} , {d0.getFullYear()}</li>
-                   </ul>
-                   <ul className="list-unstyled">
-                       <li>{dish.comments[1].comment}</li><br></br>
-                       <li>-- {dish.comments[1].author}  {months[d1.getMonth()]} {d1.getDate()} , {d1.getFullYear()}</li>
-                   </ul>
-                   <ul className="list-unstyled">
-                       <li>{dish.comments[2].comment}</li><br></br>
-                       <li>-- {dish.comments[2].author}  {months[d2.getMonth()]} {d2.getDate()} , {d2.getFullYear()}</li>
-                   </ul>
-                   <ul className="list-unstyled">
-                       <li>{dish.comments[3].comment}</li><br></br>
-                       <li>-- {dish.comments[3].author}  {months[d3.getMonth()]} {d3.getDate()} , {d3.getFullYear()}</li>
-                   </ul>
-                   <ul className="list-unstyled">
-                       <li>{dish.comments[4].comment}</li><br></br>
-                       <li>-- {dish.comments[4].author}  {months[d4.getMonth()]} {d4.getDate()} , {d4.getFullYear()}</li>
-                   </ul>
-               </div>
-           )
-    }
+function RenderComments({comments}){
+    return(
+        <div>
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {comments.map((comment) => {
+                    return(
+                        <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    )
+}
 
     const Dishdetail = (props) => {
         if (props.dish != null) {
             return (
                 <div className="container">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {props.dish.name}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish}/>
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            <RenderComments dish={props.dish}/>
+                            <RenderComments comments={props.comments}/>
                         </div>
                     </div>
                 </div>
